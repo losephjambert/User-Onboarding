f975499f878e82b6e13fb437991cd6b557fb3032
Joe <josephc.lambert@gmail.com>
2019-09-19 14:54:47 -0700

    adds remaining fields.

--- user-onboarding-client/src/components/OnboardingForm.js
+++ user-onboarding-client/src/components/OnboardingForm.js
@@ -6,7 +6,25 @@ const OnboardingForm = ({ values }) => (
   <Form>
     <div>
       <label htmlFor='name'>Name</label>
-      <Field component='input' type='text' name='name' value={values.name} />
+      <Field id='name' type='text' name='name' value={values.name} />
+    </div>
+    <div>
+      <label htmlFor='email'>Email</label>
+      <Field id='email' type='text' name='email' value={values.email} />
+    </div>
+    <div>
+      <div>
+        <label htmlFor='password'>Password</label>
+        <Field id='password' type='text' name='password' value={values.password} />
+      </div>
+      <div>
+        <label htmlFor='repeatPassword'>Repeat Password</label>
+        <Field id='repeatPassword' type='text' name='repeatPassword' value={values.repeatPassword} />
+      </div>
+    </div>
+    <div>
+      <label htmlFor='terms'>Do you agree to our Terms of Service?</label>
+      <Field id='terms' type='checkbox' name='terms' value={values.terms} />
     </div>
     <div>
       <button type='submit'>Create Profile</button>
@@ -15,9 +33,13 @@ const OnboardingForm = ({ values }) => (
 );
 
 export default withFormik({
-  mapPropsToValues({ name }) {
+  mapPropsToValues({ name, email, password, repeatPassword, terms }) {
     return {
       name: name || '',
+      email: email || '',
+      password: password || '',
+      repeatPassword: repeatPassword || '',
+      terms: terms || false,
     };
   },
   handleSubmit(values) {
