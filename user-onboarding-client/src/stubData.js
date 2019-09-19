6bf58733ca1b0a9b32b3088a950e6d65b3ff4b80
Joe <josephc.lambert@gmail.com>
2019-09-19 14:38:29 -0700

    add seed function

---
+++ user-onboarding-client/src/stubData.js
@@ -0,0 +1,20 @@
+import faker from 'faker';
+
+export default function seed(count) {
+  const users = [];
+  for (let i = 0; i < count; i++) {
+    let image = faker.image.avatar();
+    let email = faker.internet.email();
+    let name = `${faker.name.firstName()} ${faker.name.lastName()}`;
+    let role = faker.random.word();
+    let id = faker.random.uuid();
+    users.push({
+      image,
+      email,
+      name,
+      role,
+      id,
+    });
+  }
+  return users;
+}
