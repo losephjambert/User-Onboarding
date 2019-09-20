726677aa4e1c812497262bebb7b5380bc8cdf589
Joe <josephc.lambert@gmail.com>
2019-09-20 07:56:30 -0700

    meets mvp

--- user-onboarding-client/src/components/Formik.OnboardingForm.js
+++ user-onboarding-client/src/components/Formik.OnboardingForm.js
@@ -1,92 +1,70 @@
-import React from "react";
+import React from 'react';
 
-const FormikOnboardingForm = ({
-  values,
-  handleBlur,
-  handleChange,
-  handleSubmit,
-  errors,
-  touched
-}) => {
+const FormikOnboardingForm = ({ values, handleBlur, handleChange, handleSubmit, errors, touched }) => {
+  console.log(errors);
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="name">Name: </label>
-        <input
-          id="name"
-          type="text"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.name}
-          name="name"
-        />
+        <label htmlFor='name'>Name: </label>
+        <input id='name' type='text' onChange={handleChange} onBlur={handleBlur} value={values.name} name='name' />
         {touched.name && errors.name && (
-          <p style={{ display: "inline" }} className="error">
+          <p style={{ display: 'inline' }} className='error'>
             {errors.name}
           </p>
         )}
       </div>
       <div>
-        <label htmlFor="email">email: </label>
-        <input
-          id="email"
-          type="text"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.email}
-          name="email"
-        />
+        <label htmlFor='email'>email: </label>
+        <input id='email' type='text' onChange={handleChange} onBlur={handleBlur} value={values.email} name='email' />
         {touched.email && errors.email && (
-          <p style={{ display: "inline" }} className="error">
+          <p style={{ display: 'inline' }} className='error'>
             {errors.email}
           </p>
         )}
       </div>
       <div>
         <div>
-          <label htmlFor="password">Password</label>
+          <label htmlFor='password'>Password</label>
           <input
-            id="password"
-            type="text"
-            name="password"
+            id='password'
+            type='text'
+            name='password'
             value={values.password}
             onChange={handleChange}
             onBlur={handleBlur}
-            className={touched.password && errors.password && "input-error"}
+            className={touched.password && errors.password && 'input-error'}
           />
           {touched.password && errors.password && <p>{errors.password}</p>}
         </div>
         <div>
-          <label htmlFor="repeatPassword">Repeat Password</label>
+          <label htmlFor='repeatPassword'>Repeat Password</label>
           <input
-            id="repeatPassword"
-            type="text"
-            name="repeatPassword"
+            id='repeatPassword'
+            type='text'
+            name='repeatPassword'
             value={values.repeatPassword}
             onChange={handleChange}
             onBlur={handleBlur}
-            className={
-              touched.repeatPassword && errors.repeatPassword && "input-error"
-            }
+            className={touched.repeatPassword && errors.repeatPassword && 'input-error'}
           />
-          {touched.repeatPassword && errors.repeatPassword && (
-            <p>{errors.repeatPassword}</p>
-          )}
+          {touched.repeatPassword && errors.repeatPassword && <p>{errors.repeatPassword}</p>}
         </div>
       </div>
       <div>
-        <label htmlFor="terms">Do you agree to our Terms of Service?</label>
+        <label htmlFor='terms'>Do you agree to our Terms of Service?</label>
         <input
-          id="terms"
-          type="checkbox"
-          name="terms"
+          required
+          id='terms'
+          type='checkbox'
+          name='terms'
           value={values.terms}
           onChange={handleChange}
           onBlur={handleBlur}
         />
+        {errors.terms && <p>{errors.terms}</p>}
       </div>
       <div>
-        <button type="submit">Create Profile</button>
+        <button type='submit'>Create Profile</button>
       </div>
     </form>
   );
--- user-onboarding-client/src/components/Formik.OnboardingFormContainer.js
+++ user-onboarding-client/src/components/Formik.OnboardingFormContainer.js
@@ -1,46 +1,39 @@
-import React from "react";
-import { Formik } from "formik";
-import axios from "axios";
-import * as Yup from "yup";
-import FormikOnboardingForm from "./Formik.OnboardingForm";
+import React from 'react';
+import { Formik } from 'formik';
+import axios from 'axios';
+import * as Yup from 'yup';
+import FormikOnboardingForm from './Formik.OnboardingForm';
 
 const FormikOnboardingFormContainer = ({ users, addUser }) => (
   <Formik
     initialValues={{
-      name: "",
-      email: "",
-      password: "",
-      repeatPassword: "",
-      terms: false
+      name: '',
+      email: '',
+      password: '',
+      repeatPassword: '',
+      terms: false,
     }}
     onSubmit={(values, actions) => {
       axios
-        .post("https://reqres.in/api/users", values)
+        .post('https://reqres.in/api/users', values)
         .then(response => {
           console.log(response.data);
           addUser(response.data);
         })
         .catch(error => {
-          console.error("There was an error submitting your form.", error);
+          console.error('There was an error submitting your form.', error);
         });
     }}
     validationSchema={Yup.object().shape({
-      name: Yup.string().required("Please provide a name for your account"),
+      name: Yup.string().required('Please provide a name for your account'),
       email: Yup.string()
-        .email("not a properly formatted email")
-        .required("an email address is required"),
+        .email('not a properly formatted email')
+        .required('an email address is required'),
       password: Yup.string()
-        .min(12 | "Pass word must be at least 12 characters")
-        .required("please enter a valid password"),
-      repeatPassword: Yup.string().oneOf(
-        [Yup.ref("password"), null],
-        "Passwords must match"
-      ),
-      terms: Yup.bool()
-        .oneOf([true])
-        .required(
-          "You must agree to our terms of service before you can create an account"
-        )
+        .min(12 | 'Pass word must be at least 12 characters')
+        .required('please enter a valid password'),
+      repeatPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match'),
+      terms: Yup.boolean().required(),
     })}
     render={props => <FormikOnboardingForm {...props} users={users} />}
   />
